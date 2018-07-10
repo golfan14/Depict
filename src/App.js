6c949f867b601c3fdcd528ded2a528c41eca7f06
Joseph DeChant <jdechant970@gmail.com>
2018-07-10 14:59:00 -0600

    Revert "Dtelldev"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
 import Facebook from './components/Facebook';
-import MapContainer from './components/Map';
+import Map from './components/Map';
 import Carousel from './components/Carousel';
 import Footer from './components/Footer';
 
 
 class App extends Component {
-
-
-
- //log in =======================================================
   constructor(props) {
     super(props);
     this.state = {
@@ -19,35 +18,13 @@ class App extends Component {
       }
   } 
     this.logIn = this.logIn.bind(this)  
-  };
+  }
 
   logIn(user) {
     this.setState({
       user
     })
-  };
-
-// map markers====================================================
-
-
-state = {
-  markers: []
-};
-
-
-addLocation = data => {
- if (data) {
-  const marker = {
-    name: data.gmaps.formatted_address,
-    lat: data.location.lat,
-    lng: data.location.lng
   }
-  this.setState({ markers: [...this.state.markers, marker]})
- }
-  
-}
-
-
 
 
   render() {
@@ -62,19 +39,15 @@ addLocation = data => {
           {/* To get started, sign in with Facebook. */}
         </p>
         {!isLoggedIn && (<Facebook userdata={this.state.user} LogInfn={this.logIn}/>)}
-    {isLoggedIn && (<MapContainer />)}
+    {isLoggedIn && (<Map />)}
     {isLoggedIn && (<Carousel />)}
         {/* <Footer/> */}
 
-        <MapContainer addLocation = {this.addLocation} markers = {this.state.markers}/>
 
         <Footer />
       </div>
-      
     );
   }
-};
+}
 
 export default App;
-
-
